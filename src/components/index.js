a4de96adff40606d3b35eea347997889e0ff23a5
del <rodelcdavid@gmail.com>
2021-12-25 09:06:27 +0800

    initialized project components, added globalstyles

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import About from "./About/About";
+import Navbar from "./Navbar/Navbar";
+import Projects from "./Projects/Projects";
+import Contact from "./Contact/Contact";
+import Footer from "./Footer/Footer";
+
+export { Navbar, About, Projects, Contact, Footer };
